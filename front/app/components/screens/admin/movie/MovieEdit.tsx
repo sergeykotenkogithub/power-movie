59088f003c31e420c7b74cc7dbd45eba40ab660f
sergeykotenko <sergeykotenko@bk.ru>
2022-12-09 16:47:52 +0300

    Feat(Admin panel: Movie fields edit and create )

--- front/app/components/screens/admin/movie/MovieEdit.tsx
+++ front/app/components/screens/admin/movie/MovieEdit.tsx
@@ -8,6 +8,7 @@ import AdminNavigation from '@/components/ui/admin-navigation/AdminNavigation'
 import Button from '@/components/ui/form-elements/Button'
 import Field from '@/components/ui/form-elements/Field'
 import SlugField from '@/components/ui/form-elements/SlugField/SlugField'
+import UploadField from '@/components/ui/form-elements/UploadField/UploadField'
 import { Heading } from '@/components/ui/heading/Heading'
 
 import formStyles from '@/ui/form-elements/admin-form.module.scss'
@@ -16,14 +17,13 @@ import { Meta } from '@/utils/meta/Meta'
 import { generateSlug } from '@/utils/string/generateSlug'
 
 import { IMovieEditInput } from './movie-edit.interface'
+import { useAdminActors } from './useAdminActors'
+import { useAdminGenres } from './useAdminGenres'
 import { useMovieEdit } from './useMovieEdit'
 
-const DynamicTextEditor = dynamic(
-	() => import('@/ui/form-elements/TextEditor'),
-	{
-		ssr: false,
-	}
-)
+const DynamicSelect = dynamic(() => import('@/ui/select/Select'), {
+	ssr: false,
+})
 
 const MovieEdit: FC = () => {
 	const {
@@ -39,6 +39,9 @@ const MovieEdit: FC = () => {
 
 	const { isLoading, onSubmit } = useMovieEdit(setValue)
 
+	const { isLoading: isGenresLoading, data: genres } = useAdminGenres()
+	const { isLoading: isActorsLoading, data: actors } = useAdminActors()
+
 	return (
 		<Meta title="Edit movie">
 			<AdminNavigation />
@@ -50,58 +53,149 @@ const MovieEdit: FC = () => {
 					<>
 						<div className={formStyles.field}>
 							<Field
-								{...register('name', {
-									required: 'Name is required',
+								{...register('title', {
+									required: 'Title is required',
 								})}
-								placeholder="Name"
-								error={errors.name}
-								style={{ width: '31%' }}
+								placeholder="Title"
+								error={errors.title}
 							/>
 
-							<div style={{ width: '31%' }}>
-								<SlugField
-									register={register}
-									error={errors.slug}
-									generate={() => {
-										setValue('slug', generateSlug(getValues('name')))
-									}}
-								/>
-							</div>
+							<SlugField
+								register={register}
+								error={errors.slug}
+								generate={() => {
+									setValue('slug', generateSlug(getValues('title')))
+								}}
+							/>
 
 							<Field
-								{...register('icon', {
-									required: 'Icon is required',
+								{...register('parameters.country', {
+									required: 'Country is required',
 								})}
-								placeholder="Icon"
-								error={errors.icon}
+								placeholder="Country"
+								error={errors.parameters?.country}
 								style={{ width: '31%' }}
 							/>
+
+							<Field
+								{...register('parameters.duration', {
+									required: 'Duration is required',
+								})}
+								placeholder="Duration (min)"
+								error={errors.parameters?.duration}
+								style={{ width: '31%' }}
+							/>
+
+							<Field
+								{...register('parameters.year', {
+									required: 'Year is required',
+								})}
+								placeholder="Year"
+								error={errors.parameters?.year}
+								style={{ width: '31%' }}
+							/>
+
+							<Controller
+								control={control}
+								name="genres"
+								render={({ field, fieldState: { error } }) => (
+									<DynamicSelect
+										field={field}
+										options={genres || []}
+										isLoading={isGenresLoading}
+										isMulti
+										placeholder="Genres"
+										error={error}
+									/>
+								)}
+								rules={{
+									required: 'Please select at least one genre!',
+								}}
+							/>
+
+							<Controller
+								control={control}
+								name="actors"
+								render={({ field, fieldState: { error } }) => (
+									<DynamicSelect
+										field={field}
+										options={actors || []}
+										isLoading={isActorsLoading}
+										isMulti
+										placeholder="Actors"
+										error={error}
+									/>
+								)}
+								rules={{
+									required: 'Please select at least one actor!',
+								}}
+							/>
+
+							<Controller
+								control={control}
+								name="poster"
+								defaultValue=""
+								render={({
+									field: { value, onChange },
+									fieldState: { error },
+								}) => (
+									<UploadField
+										onChange={onChange}
+										value={value}
+										error={error}
+										folder="movies"
+										placeholder="Poster"
+									/>
+								)}
+								rules={{
+									required: 'Poster is required',
+								}}
+							/>
+
+							<Controller
+								control={control}
+								name="bigPoster"
+								defaultValue=""
+								render={({
+									field: { value, onChange },
+									fieldState: { error },
+								}) => (
+									<UploadField
+										onChange={onChange}
+										value={value}
+										error={error}
+										folder="movies"
+										placeholder="Big Poster"
+									/>
+								)}
+								rules={{
+									required: 'Big Poster is required',
+								}}
+							/>
+
+							<Controller
+								control={control}
+								name="videoUrl"
+								defaultValue=""
+								render={({
+									field: { value, onChange },
+									fieldState: { error },
+								}) => (
+									<UploadField
+										onChange={onChange}
+										value={value}
+										error={error}
+										folder="movies"
+										placeholder="Video"
+										style={{ marginTop: -25 }}
+										isNoImage
+									/>
+								)}
+								rules={{
+									required: 'Video is required',
+								}}
+							/>
 						</div>
-
-						<Controller
-							control={control}
-							name="description"
-							defaultValue=""
-							render={({
-								field: { value, onChange },
-								fieldState: { error },
-							}) => (
-								<DynamicTextEditor
-									onChange={onChange}
-									value={value}
-									error={error}
-									placeholder="Description"
-								/>
-							)}
-							rules={{
-								validate: {
-									required: (v) =>
-										(v && stripHtml(v).result.length > 0) ||
-										'Description is required',
-								},
-							}}
-						/>
-
 						<Button>Update</Button>
 					</>
 				)}
---
+++ front/app/components/screens/admin/movie/useAdminGenres.ts
@@ -0,0 +1,25 @@
+import { useQuery } from 'react-query'
+
+import { IOption } from '@/components/ui/select/select.interface'
+
+import { GenreService } from '@/services/genre.service'
+
+import { toastError } from '@/utils/toast-error'
+
+export const useAdminGenres = () => {
+	const queryData = useQuery('List of genre', () => GenreService.getAll(), {
+		select: ({ data }) =>
+			data.map(
+				(genre): IOption => ({
+					label: genre.name,
+					value: genre._id,
+				})
+			),
+
+		onError: (error) => {
+			toastError(error, 'Actor List')
+		},
+	})
+
+	return queryData
+}
